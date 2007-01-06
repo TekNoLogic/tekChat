6dbec660063db80cef20ab69621a974ad0b5590e
Tekkub Stoutwrithe <tekkub@gmail.com>
2007-01-06 12:49:28 +0000

    tekChat - Fixed to avoid tainting
    git-svn-id: http://tekkub-wow.googlecode.com/svn/trunk/tekChat@128 86fe6d9a-1522-0410-a387-bf9db416f0a0

--- Desalivate.lua
+++ Desalivate.lua
@@ -1,8 +1,8 @@
 ﻿
-local oldOnEvent = ChatFrame_OnEvent
-
-ChatFrame_OnEvent = function (event)
-	if event == "CHAT_MSG_TEXT_EMOTE" and arg1 and string.find(arg1, "spit") then return end
-	return oldOnEvent(event)
+local oldOnEvent = ChatFrame_MessageEventHandler
+local orig = ChatFrame_MessageEventHandler
+ChatFrame_MessageEventHandler = function(event, a1, ...)
+	if event == "CHAT_MSG_TEXT_EMOTE" and a1 and string.find(a1, "spit") then return end
+	return orig(event, a1, ...)
 end
 
--- NoSayLinks.lua
+++ NoSayLinks.lua
@@ -1,8 +1,8 @@
 ﻿
-GFY_WoW_ChatFrame_OnEvent = ChatFrame_OnEvent
-ChatFrame_OnEvent = function(event)
-	if event == "CHAT_MSG_SAY" and string.find(arg1, "^|c.+|Hitem.+|h.+|h|r$") then return end
-	return GFY_WoW_ChatFrame_OnEvent(event)
+local orig = ChatFrame_MessageEventHandler
+ChatFrame_MessageEventHandler = function(event, a1, ...)
+	if event == "CHAT_MSG_SAY" and string.find(a1, "^|c.+|Hitem.+|h.+|h|r$") then return end
+	return orig(event, a1, ...)
 end
 
 
