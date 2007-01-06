16d9aa5a6b501a15fd6339d1f6d3886c59540888
Tekkub Stoutwrithe <tekkub@gmail.com>
2007-01-06 22:44:21 +0000

    tekChat - Tweaks to binding stuffs, and other crap
    git-svn-id: http://tekkub-wow.googlecode.com/svn/trunk/tekChat@135 86fe6d9a-1522-0410-a387-bf9db416f0a0

--- ChatTypeBindings.lua
+++ ChatTypeBindings.lua
@@ -1,21 +1,20 @@
 ﻿
-if not ConglomoMod then ConglomoMod = {} end
 
---BINDING_HEADER_CONGLOMOMOD = "Chat Type Key Bindings"
-BINDING_NAME_CONGLOMOMOD_MYDEBUG = "myDebug"
-BINDING_NAME_CONGLOMOMOD_SAY = "Say"
-BINDING_NAME_CONGLOMOMOD_PARTY = "Party"
-BINDING_NAME_CONGLOMOMOD_GUILD = "Guild"
-BINDING_NAME_CONGLOMOMOD_RAID = "Raid"
-BINDING_NAME_CONGLOMOMOD_OFFICER = "Officer"
-BINDING_NAME_CONGLOMOMOD_SCRIPT = "Script"
 
-function ConglomoMod:OpenChatType(ctype)
-  DEFAULT_CHAT_FRAME.editBox.chatType = ctype
-  ChatEdit_UpdateHeader(DEFAULT_CHAT_FRAME.editBox)
-  DEFAULT_CHAT_FRAME.editBox:Show()
-  DEFAULT_CHAT_FRAME.editBox.setText = 1
-  DEFAULT_CHAT_FRAME.editBox.text = ""
+--BINDING_HEADER_TEKCHAT = "Chat Type Key Bindings"
+BINDING_NAME_TEKCHAT_SAY = "Say"
+BINDING_NAME_TEKCHAT_PARTY = "Party"
+BINDING_NAME_TEKCHAT_GUILD = "Guild"
+BINDING_NAME_TEKCHAT_RAID = "Raid"
+BINDING_NAME_TEKCHAT_OFFICER = "Officer"
+BINDING_NAME_TEKCHAT_SCRIPT = "Script"
+
+function TekChat_OpenChatType(ctype)
+	DEFAULT_CHAT_FRAME.editBox.chatType = ctype
+	ChatEdit_UpdateHeader(DEFAULT_CHAT_FRAME.editBox)
+	DEFAULT_CHAT_FRAME.editBox:Show()
+	DEFAULT_CHAT_FRAME.editBox.setText = 1
+	DEFAULT_CHAT_FRAME.editBox.text = ""
 end
 
 
--- Desalivate.lua
+++ Desalivate.lua
@@ -1,5 +1,4 @@
 ﻿
-local oldOnEvent = ChatFrame_MessageEventHandler
 local orig = ChatFrame_MessageEventHandler
 ChatFrame_MessageEventHandler = function(event, a1, ...)
 	if event == "CHAT_MSG_TEXT_EMOTE" and a1 and string.find(a1, "spit") then return end
