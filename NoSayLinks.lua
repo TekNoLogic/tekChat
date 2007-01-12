7e24da25da5c0a1dad8dd9c9332dfd260e98c34c
Tekkub Stoutwrithe <tekkub@gmail.com>
2007-01-12 09:23:54 +0000

    tekChat - fixed bug in NoSayLinks
    git-svn-id: http://tekkub-wow.googlecode.com/svn/trunk/tekChat@164 86fe6d9a-1522-0410-a387-bf9db416f0a0

--- NoSayLinks.lua
+++ NoSayLinks.lua
@@ -1,8 +1,8 @@
 ﻿
 local orig = ChatFrame_MessageEventHandler
-ChatFrame_MessageEventHandler = function(event, a1, ...)
+ChatFrame_MessageEventHandler = function(a1, ...)
 	if event == "CHAT_MSG_SAY" and string.find(a1, "^|c.+|Hitem.+|h.+|h|r$") then return end
-	return orig(event, a1, ...)
+	return orig(a1, ...)
 end
 
 
