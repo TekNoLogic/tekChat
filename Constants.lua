9656be00b43d5cb6553950d43645dcb32cee9d82
Tekkub Stoutwrithe <tekkub@gmail.com>
2007-01-28 15:58:34 +0000

    tekChat - Adding editbox moving, sticky channels
    git-svn-id: http://tekkub-wow.googlecode.com/svn/trunk/tekChat@223 86fe6d9a-1522-0410-a387-bf9db416f0a0

--- Constants.lua
+++ Constants.lua
@@ -1,10 +1,27 @@
 ﻿
-CHAT_FLAG_AFK = "[AFK] ";
-CHAT_FLAG_DND = "[DND] ";
-CHAT_FLAG_GM = "[GM] ";
---~~ CHAT_GUILD_GET = "%s:\32"; -- Guild message from player %s
---~~ CHAT_RAID_GET = "%s:\32"; -- Raid message from player %s
---~~ CHAT_PARTY_GET = "%s:\32"; -- Party message from player %s
-CHAT_WHISPER_GET = "%s:\32"; -- Whisper from player %s
-CHAT_WHISPER_INFORM_GET = "To %s:\32"; -- A whisper already sent to player %s
+CHAT_FLAG_AFK = "[AFK] "
+CHAT_FLAG_DND = "[DND] "
+CHAT_FLAG_GM = "[GM] "
+CHAT_GUILD_GET = "%s:\32" -- Guild message from player %s
+CHAT_RAID_GET = "%s:\32" -- Raid message from player %s
+CHAT_PARTY_GET = "%s:\32" -- Party message from player %s
+CHAT_WHISPER_GET = "%s:\32" -- Whisper from player %s
+CHAT_WHISPER_INFORM_GET = "To %s:\32" -- A whisper already sent to player %s
 
+ChatTypeInfo["OFFICER"].sticky = 1
+ChatTypeInfo["YELL"].sticky = 1
+ChatTypeInfo["CHANNEL"].sticky = 1
+
+--~ LOOT_ROLL_ALL_PASSED = "Everyone |cff808080passed|r on: %s|Hitem:%d:%d:%d:%d|h[%s]|h%s"
+--~ LOOT_ROLL_PASSED = "%s |cff808080passed|r on: %s|Hitem:%d:%d:%d:%d|h[%s]|h%s"
+--~ LOOT_ROLL_PASSED_SELF = "You |cff808080passed|r on: %s|Hitem:%d:%d:%d:%d|h[%s]|h%s"
+--~ LOOT_ROLL_GREED = "%s has selected |cffFFFF00Greed|r for: %s|Hitem:%d:%d:%d:%d|h[%s]|h%s"
+--~ LOOT_ROLL_GREED_SELF = "You have selected |cffFFFF00Greed|r for: %s|Hitem:%d:%d:%d:%d|h[%s]|h%s"
+--~ LOOT_ROLL_ROLLED_GREED = "|cffFFFF00Greed Roll|r - %d for %s|Hitem:%d:%d:%d:%d|h[%s]|h%s by %s"
+--~ LOOT_ROLL_ROLLED_GREED_SELF = "|cffFFFF00You roll a %d (Greed) on:|r %s|Hitem:%d:%d:%d:%d|h[%s]|h%s"
+--~ LOOT_ROLL_NEED = "%s has selected |cffFF0000Need|r for: %s|Hitem:%d:%d:%d:%d|h[%s]|h%s"
+--~ LOOT_ROLL_NEED_SELF = "You have selected |cffFF0000Need|r for: %s|Hitem:%d:%d:%d:%d|h[%s]|h%s"
+--~ LOOT_ROLL_ROLLED_NEED = "|cffFF0000Need Roll|r - %d for %s|Hitem:%d:%d:%d:%d|h[%s]|h%s by %s"
+--~ LOOT_ROLL_ROLLED_NEED_SELF = "|cffFF0000You roll a %d (Need) on:|r %s|Hitem:%d:%d:%d:%d|h[%s]|h%s"
+--~ LOOT_ROLL_WON = "|cffffff80%s won:|r %s|Hitem:%d:%d:%d:%d|h[%s]|h%s"
+--~ LOOT_ROLL_YOU_WON = "|cffffff80You won:|r %s|Hitem:%d:%d:%d:%d|h[%s]|h%s"
---
+++ Editbox.lua
@@ -0,0 +1,7 @@
+﻿
+local eb = ChatFrameEditBox
+eb:ClearAllPoints()
+eb:SetPoint("BOTTOMLEFT",  ChatFrame1, "TOPLEFT",  -5, 0)
+eb:SetPoint("BOTTOMRIGHT", ChatFrame1, "TOPRIGHT", 5, 0)
+eb:SetAltArrowKeyMode(false)
+
