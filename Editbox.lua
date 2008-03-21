71c2c74b80b10f0ff29c026393424e214394a463
Tekkub Stoutwrithe <tekkub@gmail.com>
2008-03-21 14:43:26 -0600

    Relocate editbox

--- Editbox.lua
+++ Editbox.lua
@@ -1,7 +1,7 @@
 ﻿
 local eb = ChatFrameEditBox
 eb:ClearAllPoints()
-eb:SetPoint("BOTTOMLEFT",  ChatFrame1, "TOPLEFT",  -5, 0)
-eb:SetPoint("BOTTOMRIGHT", ChatFrame1, "TOPRIGHT", 5, 0)
+eb:SetPoint("TOPLEFT",  ChatFrame1, "TOPLEFT",  -5, 4)
+eb:SetPoint("TOPRIGHT", ChatFrame1, "TOPRIGHT", 5, 4)
 eb:SetAltArrowKeyMode(false)
 
