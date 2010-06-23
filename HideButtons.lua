4aa0fbeae44b79e491bf12ee0d79f0825d32aaff
tekkub <tekkub@gmail.com>
2010-06-23 03:18:09 -0600

    The buttons changed a bit

--- HideButtons.lua
+++ HideButtons.lua
@@ -1,15 +1,14 @@
 ﻿
-local function emptyfunc() end
-local buttons = {"UpButton", "DownButton", "BottomButton"}
-local _G = getfenv(0)
-
 ChatFrameMenuButton:Hide()
+FriendsMicroButton:Hide()
 
 for i=1,NUM_CHAT_WINDOWS do
-	for _,j in pairs(buttons) do
-		local f = _G["ChatFrame"..i..j]
-		if f == nil then ChatFrame1:AddMessage("WTF???  ChatFrame"..i..j) end
-		f:Hide()
-		f.Show = emptyfunc
-	end
+	_G["ChatFrame"..i]:SetClampRectInsets(0,0,0,0)
+	local f = _G["ChatFrame"..i.."ButtonFrame"]
+	f:Hide()
+	f.Show = f.Hide
+
+	local tab = _G["ChatFrame"..i.."Tab"]
+	tab.noMouseAlpha = 0
+	tab:SetAlpha(0)
 end
