dee739b6c7ceda8002161199d4b2a3421f2599ca
tekkub <tekkub@gmail.com>
2010-06-23 03:18:45 -0600

    We have many editboxes now, preserve the old style

--- Editbox.lua
+++ Editbox.lua
@@ -1,7 +1,12 @@
 ﻿
-local eb = ChatFrameEditBox
-eb:ClearAllPoints()
-eb:SetPoint("TOPLEFT",  ChatFrame1, "TOPLEFT",  -5, 4)
-eb:SetPoint("TOPRIGHT", ChatFrame1, "TOPRIGHT", 5, 4)
-eb:SetAltArrowKeyMode(false)
+for i=1,NUM_CHAT_WINDOWS do
+	local eb = _G["ChatFrame"..i.."EditBox"]
+	eb:ClearAllPoints()
+	eb:SetPoint("TOPLEFT",  ChatFrame1, "TOPLEFT",  -5, 8)
+	eb:SetPoint("TOPRIGHT", ChatFrame1, "TOPRIGHT", 5, 8)
+	eb:SetAltArrowKeyMode(false)
 
+	_G["ChatFrame"..i.."EditBoxLeft"]:Hide()
+	_G["ChatFrame"..i.."EditBoxMid"]:Hide()
+	_G["ChatFrame"..i.."EditBoxRight"]:Hide()
+end
