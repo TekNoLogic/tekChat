e4345d9c720aed43acbbfed7211b9fc8c70c358b
tekkub <tekkub@gmail.com>
2010-11-28 19:09:47 -0700

    Fix editbox background

--- Editbox.lua
+++ Editbox.lua
@@ -1,10 +1,16 @@
 ﻿
+local bg = {bgFile = "Interface\\Tooltips\\UI-Tooltip-Background", insets = {left = 5, right = 5, top = 5, bottom = 5}, tile = true, tileSize = 16}
+
 for i=1,NUM_CHAT_WINDOWS do
 	local eb = _G["ChatFrame"..i.."EditBox"]
 	eb:ClearAllPoints()
 	eb:SetPoint("TOPLEFT",  ChatFrame1, "TOPLEFT",  -5, 8)
 	eb:SetPoint("TOPRIGHT", ChatFrame1, "TOPRIGHT", 5, 8)
 	eb:SetAltArrowKeyMode(false)
+
+	eb:SetBackdrop(bg)
+	eb:SetBackdropColor(0.09, 0.09, 0.19, 0.95)
+	eb:SetBackdropBorderColor(0.5, 0.5, 0.5, 1)
 
 	_G["ChatFrame"..i.."EditBoxLeft"]:Hide()
 	_G["ChatFrame"..i.."EditBoxMid"]:Hide()
